4573eb90dc255763f38a7ff1a606c636e7acd685
Jeovando Júnior <139470590+JeovandoJr@users.noreply.github.com>
2023-08-04 22:19:05 +0000

    Adicionada a Classe Imagem

--- main.py
+++ main.py
@@ -23,5 +23,35 @@ class Download:
 
         except requests.exceptions.RequestException as e:
             print(f"Erro na Conexão: {e}")
+
+# Classe para representar um arquivo de imagem, .jpg ou .png
+class Imagem:
+    imagem = None
+
+    def __init__(self, id, nome_arquivo, destino_arquivo):
+        self.id = id
+        self.nome_arquivo = nome_arquivo
+        self.local_referencia = destino_arquivo
+
+        try:
+            self.imagem = Image.open(destino_arquivo)
+        except Exception as ex:
+            print(f"Erro ao criar imagem com o arquivo {nome_arquivo} na referência {destino_arquivo}: {str(str)}")
+
+    def dimensoes(self):
+        return self.imagem.size
+    
+    def tamanho(self):
+        return os.path.getsize(self.local_referencia)
+
+    def formato(self):
+        return self.imagem.format
+    
+    def conteudo(self):
+        return self.imagem
+    
+    def __str__(self):
+        return f"Nome: {self.nome_arquivo}, Dimensões: {self.dimensoes}, Formato: {self.formato()}, Tamanho: {self.tamanho()} Bytes"
+
         
 
